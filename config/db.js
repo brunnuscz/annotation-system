c13475b3e7537e6e454e8814fa304aa56621a12f
Bruno Cruz <brunocruz012013@gmail.com>
2021-10-05 17:03:40 -0300

    Configurando conexão

--- config/db.js
+++ config/db.js
@@ -1,8 +1,9 @@
 // VERIFICAR SE TA RODANDO A APLICAÇÃO NO HEROKU 
 // COM O MONGODB ATLAS OU BANCO DE DADOS LOCAL
-    if(process.env.NODE_ENV == "production"){
-        module.exports = {mongoURI: "mongodb+srv://brunnuscz:<pudim0611>@annotation-system.pn9mh.mongodb.net/annotation-system?retryWrites=true&w=majority"}
-    }else{
-        module.exports = {mongoURI: "mongodb://localhost/annotation-system"}
-    }
+    
+if(process.env.NODE_ENV == "production"){
+    module.exports = {mongoURI: "mongodb+srv://brunnuscz:pudim0611@annotation-system.pn9mh.mongodb.net/annotation-system?retryWrites=true&w=majority"}
+}else{
+    module.exports = {mongoURI: "mongodb://localhost/annotation-system"}
+}
 
